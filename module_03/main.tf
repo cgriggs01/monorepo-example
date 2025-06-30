42e4ab249f1b2f61d93c552fa0c5bb08255b7a6c
Chris Griggs <cgriggs@hashicorp.com>
2025-06-30 09:25:14 -0700

    Update main.tf

--- module_03/main.tf
+++ module_03/main.tf
@@ -15,3 +15,7 @@ output "null_resource_id" {
   description = "The `id` of the `null_resource` resource in this module."
   value       = "${null_resource.resource.id}"
 }
+
+module "null_module_01" {
+  source = "../module_01"
+}
